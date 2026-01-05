9449e4771ef1b7b8aa7950aeef8d59bda1571f18
ikatinas <ikatinas@yahoo.com>
2026-01-05 20:15:42 +0200

    tooltip info - Growable in garden

--- src/script.ts
+++ src/script.ts
@@ -189,6 +189,24 @@ function buildIngredientTooltipText(ingredient: IngredientData): HTMLElement {
     }
   }
 
+  if (ingredient.garden != null) {
+    container.appendChild(document.createElement('br'));
+    const gardenDiv = document.createElement('div');
+    gardenDiv.style.display = 'flex';
+    gardenDiv.style.alignItems = 'center';
+    gardenDiv.style.gap = '4px';
+    gardenDiv.innerHTML = `
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#0b7609ff" width="14" height="14">
+        <g>
+          <path fill="none" d="M0 0H24V24H0z"></path>
+          <path d="M21 3v2c0 3.866-3.134 7-7 7h-1v1h5v7c0 1.105-.895 2-2 2H8c-1.105 0-2-.895-2-2v-7h5v-3c0-3.866 3.134-7 7-7h3zM5.5 2c2.529 0 4.765 1.251 6.124 3.169C10.604 6.51 10 8.185 10 10v1h-.5C5.358 11 2 7.642 2 3.5V2h3.5z"></path>
+        </g>
+      </svg>
+      <span>Growable in garden</span>
+    `.trim();
+    container.appendChild(gardenDiv);
+  }
+
   const collectedBy = (ingredient.collected_by ?? '').trim();
   if (collectedBy) {
     container.appendChild(document.createElement('br'));
